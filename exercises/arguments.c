d62305ea22fcc5c59a4031b197c7d030ade9f05d
Anthony Brice <anthonybrice@lateachiever.com>
2016-04-29 00:13:13 -0700

    fix arguments example

--- exercises/arguments.c
+++ exercises/arguments.c
@@ -1,15 +1,20 @@
 // arguments.c
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    printf("Usage: %s <name>\n", argv[0]);
+  if (argc < 3) {
+    printf("Usage: %s <name> <integer>\n", argv[0]);
     return -1;
   }
 
   // YOUR TASK: Read the user's name from command line arguments and print it.
+  // Then read a number from the command line.
 
-  printf("Hello, %s\n", argv[1]);
-  return 0;
+  char* name = argv[1];
+  int num = atoi(argv[2]);
+
+  printf("Hello, %s\n", name);
+  printf("Your magic number is %d\n", num);
 }
